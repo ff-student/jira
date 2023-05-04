20ccc4b1e0a68c619ee6083e6b6f8a0be61666bb
fan <634719176@qq.com>
2023-05-04 19:59:03 +0800

    处理me接口: loading和error

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -3,6 +3,8 @@ import * as auth from "auth_provider";
 import { User } from "screens/project-list/search-panel";
 import { http } from "utils/http";
 import { useMount } from "utils";
+import { useAsync } from "utils/use-async";
+import { FullPageErrorFallbak, FullPageLoading } from "components/lib";
 
 interface AuthForm {
   username: string;
@@ -32,15 +34,31 @@ const AuthContext = React.createContext<
 AuthContext.displayName = "AuthContext";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
 
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
 
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
   });
+  //当判断是否有token时显示加载中
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+  //全局错误提升
+  if (isError) {
+    return <FullPageErrorFallbak error={error}></FullPageErrorFallbak>;
+  }
 
   return (
     <AuthContext.Provider
