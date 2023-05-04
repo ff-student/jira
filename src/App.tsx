dcaa2a85c2a1aa6cc94809a58ac856e7ab3e9816
fan <634719176@qq.com>
2023-05-04 20:31:04 +0800

    添加: errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { useAuth } from "context/auth-context";
 import "./App.css";
 import { AuthenticatedApp } from "authenticated-app";
 import { UnauthenticatedApp } from "unauthenticated-app";
+import { FullPageErrorFallbak } from "components/lib";
+import { ErrorBoundary } from "components/errot-boundary";
 
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      {/* <ProjectListScreen></ProjectListScreen> */}
-      {/*  <TsReactTest></TsReactTest> */}
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallbak}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
